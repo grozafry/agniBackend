1e8bf8d31134b79fd212afd7773e442b5faaaf59
surajbahugunalc <surajb@linecraft.ai>
2024-09-18 18:06:52 +0530

    anbc

--- app.py
+++ app.py
@@ -492,7 +492,7 @@ def get_ai_comments(pr_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=7000)
 
 
 
